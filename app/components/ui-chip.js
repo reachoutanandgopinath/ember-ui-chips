b5c5cb34df77139e4b3bda54d84df7c6a991e50a
Anand Gopinath <reachoutanandgopinath@gmail.com>
2023-01-29 11:35:16 +0530

    Changes on adding basic ember addon struture

---
+++ app/components/ui-chip.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-ui-chips/components/ui-chip';
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const moreEnthusiasm = function (phrase) {
+  return phrase + '!!!';
+}
+
+export { moreEnthusiasm }
 module.exports = {
   name: require('./package').name,
+  moreEnthusiasm: { moreEnthusiasm }
 };
