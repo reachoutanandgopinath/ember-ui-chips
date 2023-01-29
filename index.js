1fa833abe08003ad36af4357a9c7e80af03babb6
Anand Gopinath <reachoutanandgopinath@gmail.com>
2023-01-29 12:05:06 +0530

    changes on index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const moreEnthusiasm = function (phrase) {
-  return phrase + '!!!';
-}
-
-export { moreEnthusiasm }
 module.exports = {
   name: require('./package').name,
-  moreEnthusiasm: { moreEnthusiasm }
 };
